fe42734ee2d77db195cc8d422b8247365bff26db
Z08486 <taoshaoyuan@hozonauto.com>
2023-10-18 20:25:07 +0800

    Add GlobalHdMap. URL: http://qingluan.hozonauto.com/geelib/repoMergeDetail?repoId=algorithm/mapping&mergeId=114

---
+++ modules/map_fusion/include/map_fusion/map_service/global_hd_map.h
@@ -0,0 +1,40 @@
+/******************************************************************************
+ *   Copyright (C) 2021 HOZON-AUTO Ltd. All rights reserved.
+ *   file       ： global_hd_map.h
+ *   author     ： mashaoping
+ *   date       ： 2023.10
+ ******************************************************************************/
+
+#pragma once
+
+#include <depend/map/hdmap/hdmap.h>
+
+namespace hozon {
+namespace mp {
+
+class GlobalHdMap {
+ public:
+  int Init();
+  std::shared_ptr<hdmap::HDMap> GetHdMap();
+
+ private:
+  std::shared_ptr<hdmap::HDMap> hd_map_ = nullptr;
+
+  // declare singleton
+ private:
+  GlobalHdMap() = default;
+  ~GlobalHdMap() = default;
+  GlobalHdMap(const GlobalHdMap&);
+  GlobalHdMap& operator=(const GlobalHdMap&);
+
+ public:
+  static GlobalHdMap& Instance() {
+    static GlobalHdMap instance;
+    return instance;
+  }
+};
+
+#define GLOBAL_HD_MAP hozon::mp::GlobalHdMap::Instance().GetHdMap()
+
+}  // namespace mp
+}  // namespace hozon
---
+++ modules/map_fusion/src/map_service/global_hd_map.cc
@@ -0,0 +1,23 @@
+/******************************************************************************
+ *   Copyright (C) 2021 HOZON-AUTO Ltd. All rights reserved.
+ *   file       ： global_hd_map.cc
+ *   author     ： mashaoping
+ *   date       ： 2023.10
+ ******************************************************************************/
+
+#pragma once
+
+#include "map_fusion/map_service/global_hd_map.h"
+
+namespace hozon {
+namespace mp {
+
+int GlobalHdMap::Init() {
+  hd_map_ = std::make_shared<hdmap::HDMap>();
+  return 0;
+}
+
+std::shared_ptr<hdmap::HDMap> GlobalHdMap::GetHdMap() { return hd_map_; }
+
+}  // namespace mp
+}  // namespace hozon

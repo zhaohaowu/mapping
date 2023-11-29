bc3d58c5caade0c8400d5d2c400f6b62059adbb6
xuliang <xuliang@hozonauto.com>
2023-11-29 16:11:53 +0800

    enable map fusion on adf lite

---
+++ onboard/onboard_lite/map_fusion/map_fusion_config_lite.cc
@@ -0,0 +1,15 @@
+/******************************************************************************
+ *   Copyright (C) 2023 HOZON-AUTO Ltd. All rights reserved.
+ *   file       ： map_fusion_config_lite.cc
+ *   author     ： xuliang
+ *   date       ： 2023.11
+ ******************************************************************************/
+#pragma once
+
+#include <gflags/gflags.h>
+
+#include "onboard/onboard_lite/map_fusion/map_fusion_config_lite.h"
+
+DEFINE_bool(orin_viz, false, "if use rviz");
+DEFINE_string(orin_viz_addr, "tcp://10.6.73.235:9100",
+              "RvizAgent's working address");

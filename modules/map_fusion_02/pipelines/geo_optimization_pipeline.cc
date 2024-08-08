3aa7fbb4d9b1d3bb5fa51f9b489486e007d2e0f3
80044669 <chenlongxi@hozonauto.com>
2024-08-08 09:09:40 +0800

    add CMakeLists.txt

---
+++ modules/map_fusion_02/pipelines/geo_optimization_pipeline.cc
@@ -0,0 +1,8 @@
+/******************************************************************************
+ *   Copyright (C) 2023 HOZON-AUTO Ltd. All rights reserved.
+ *   file       ： geo_optimization_pipeline.cc
+ *   author     ： chenlongxi
+ *   date       ： 2023.09
+ ******************************************************************************/
+
+#include "modules/map_fusion_02/pipelines/geo_optimization_pipeline.h"

d5454348e15b1ca5bc10f4dca59feaa2c0d12633
80044669 <chenlongxi@hozonauto.com>
2024-08-16 11:27:27 +0800

    move some template func to calc_util

--- modules/map_fusion_02/common/calc_util.h
+++ modules/map_fusion_02/common/calc_util.h
@@ -32,90 +32,27 @@ inline double DegToRad(double deg) { return deg * M_PI / 180; }
 inline double RadToDeg(double rad) { return 180 * rad / M_PI; }
 
 template <typename T, typename M>
-T CubicResolve(const std::vector<T>& coefs, M t) {
-  int order = coefs.size();
-  switch (order) {
-    case 4: {
-      return ((coefs[3] * t + coefs[2]) * t + coefs[1]) * t + coefs[0];
-    }
-    case 3: {
-      return (coefs[2] * t + coefs[1]) * t + coefs[0];
-    }
-    case 2: {
-      return coefs[1] * t + coefs[0];
-    }
-    case 1: {
-      return coefs[0];
-    }
-    default:
-      return 0.0;
-  }
-}
+T CubicResolve(const std::vector<T>& coefs, M t);
 
 template <typename T>
-bool InRange(T x, T min_x, T max_x) {
-  return (x >= min_x && x <= max_x);
-}
+bool InRange(T x, T min_x, T max_x);
 
 template <typename T>
-bool OutRange(T x, T min_x, T max_x) {
-  return (x < min_x || x > max_x);
-}
+bool OutRange(T x, T min_x, T max_x);
 
 template <typename T>
-bool InCubicRange(T x, const LineCubic& cubic) {
-  return x >= cubic.start && x <= cubic.end;
-}
-
+bool InCubicRange(T x, const LineCubic& cubic);
 // 采样三次多项式曲线
 template <typename T>
 void SamplingCubic(const LineCubic& cubic, float step,
-                   std::vector<Eigen::Matrix<T, 3, 1>>* pts) {
-  if (pts == nullptr) return;
-
-  pts->clear();
-  float x = cubic.start;
-  while (x < cubic.end) {
-    float y = CubicResolve(cubic.coefs, x);
-    Eigen::Matrix<T, 3, 1> pt(x, y, 0);
-    pts->emplace_back(pt);
-
-    x += step;
-  }
-  x = cubic.end;
-  float end_y = CubicResolve(cubic.coefs, x);
-  pts->emplace_back(x, end_y, 0);
-}
+                   std::vector<Eigen::Matrix<T, 3, 1>>* pts);
 
 template <typename pointType>
-double GetLength(const std::vector<pointType>& point_sets) {
-  double total_length = 0;
-  // 遍历point_sets中的点
-  for (size_t i = 1; i < point_sets.size(); ++i) {
-    // 计算车体系两点之间的欧式距离
-    double dx = point_sets[i].x() - point_sets[i - 1].x();
-    double dy = point_sets[i].y() - point_sets[i - 1].y();
-    double length = std::sqrt(dx * dx + dy * dy);
-
-    // 累加得到整个车道线的长度
-    total_length += length;
-  }
-  return total_length;
-}
+double GetLength(const std::vector<pointType>& point_sets);
 
 template <typename pointType>
 float GetOverLayRatioBetweenTwoLane(const std::vector<pointType>& curve1,
-                                    const std::vector<pointType>& curve2) {
-  auto length1 = GetLength(curve1);
-  auto length2 = GetLength(curve2);
-
-  auto overlay_start = std::max(curve1.front().x(), curve2.front().x());
-  auto overlay_end = std::min(curve1.back().x(), curve2.back().x());
-  auto overlay_length =
-      overlay_end - overlay_start > 0 ? overlay_end - overlay_start : 0;
-
-  return overlay_length / std::min(length1, length2);
-}
+                                    const std::vector<pointType>& curve2);
 
 double NowInSec();
 
